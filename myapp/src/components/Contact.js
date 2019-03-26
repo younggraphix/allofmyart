65a1d3fa61deb73167cce5a6cfaba54ba883f025
Austin <austin@ipartnr.com>
2019-03-25 22:41:47 -0600

    resized images

--- myapp/src/components/Contact.js
+++ myapp/src/components/Contact.js
@@ -3,8 +3,68 @@ import React, { Component } from 'react';
 class Contact extends Component {
     render() {
       return (
-          <div>
-              <a href="mailto:austin@younggraphix.com" target="_top">Contact Me</a>
+          <div class="contact">
+              <form>
+                  <fieldset>
+                      <legend>Personal Info</legend>
+                      <div>
+                        <label>First Name</label>
+                        <input type="text"/>
+                      </div>
+                      <div>
+                        <label>Last Name</label>
+                        <input type="text"/>
+                      </div>
+                      <div>
+                        <label>Email</label>
+                        <input type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"/>
+                      </div>
+                      <div>
+                        <label>Phone</label>
+                        <input type="tel"/>
+                      </div>
+                        <input type="hidden" name="" value=""/>
+                        <input type="hidden" name="" value=""/>
+                  </fieldset>
+                  <fieldset>
+                      <legend>Request</legend>
+                      <div>
+                        <label>What is your project?</label>
+                        <textarea type="text"/>
+                      </div>
+                      <div class="radios">
+                        <label>Does it involve site creation?</label>
+                        <label>yes</label>
+                        <input type="radio" name="site-creation-yes" value="yes" />
+                        <label>no</label>
+                        <input type="radio" name="site-creation-no" value="no" />
+                      </div>
+                      <div class="radios animation">
+                        <label>Does it require animation?</label>
+                        <label>yes</label>
+                        <input type="radio" name="site-creation-yes" value="yes" />
+                        <label>no</label>
+                        <input type="radio" name="site-creation-no" value="no" />
+                      </div>
+                  </fieldset>
+                  <fieldset>
+                      <legend>Inspiration</legend>
+                      <div>
+                        <label>What other brands inspire you?</label>
+                        <textarea type="text"/>
+                      </div>
+                      <div>
+                        <label>Where did you hear about me?</label>
+                        <select name="" id="">
+                            <option value="friend">a friend</option>
+                            <option value="instagram">instagram</option>
+                            <option value="other">other</option>
+                        </select>
+                      </div>
+                  </fieldset>
+                  <button>Send Request</button>
+              </form>
+              {/* <a href="mailto:austin@younggraphix.com" target="_top">Contact Me</a> */}
           </div>
       )
     }
